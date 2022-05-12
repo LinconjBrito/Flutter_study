feed2a584cc1bd4f80079caaf27b8b5cccdc3eb8
LinconjBrito <linconjbrito@gmail.com>
2022-05-12 10:04:50 -0300

    Nova aula sobre outra forma de escrever funções de uma forma mais exuta

---
+++ project/funcao_em_variavel2.dart
@@ -0,0 +1,14 @@
+main() {
+  var adicao = (int a, int b) {
+    return a + b;
+  };
+
+  var subtracao = (int a, int b) => a - b;
+  var multiplicacao = (int a, int b) => a * b;
+  var divisao = (int a, int b) => a / b;
+
+  print(subtracao(10, 5));
+  print(adicao(10, 5));
+  print(multiplicacao(10, 5));
+  print(divisao(20, 5));
+}
