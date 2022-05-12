0fce77cff3ea20069d7ab6276eff6628c3a92900
LinconjBrito <linconjbrito@gmail.com>
2022-05-12 08:20:37 -0300

    Um commit de test

---
+++ Untitled-1.dart
@@ -0,0 +1,19 @@
+import 'dart:math';
+
+main() {
+  int n1 = numeroAleatorio(100);
+  print(n1);
+
+  int n2 = numeroAleatorio();
+  print(n2);
+
+  imprimirData(03, 2015);
+}
+
+int numeroAleatorio([int maximo = 11]) {
+  return Random().nextInt(maximo);
+}
+
+imprimirData(int dia, [int mes = 1, int ano = 1970]) {
+  print('$dia/$mes/$ano');
+}
